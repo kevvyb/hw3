97976277c2001e7aaa0ddfa3c4458d55f299014b
Kevin Bickard <kbick4@gmail.com>
2021-04-17 11:34:34 -0500

    initial commit

--- noober.js
+++ noober.js
@@ -13,5 +13,42 @@ document.addEventListener('DOMContentLoaded', async function() {
   // 🔥 YOUR CODE GOES HERE 🔥
   // Write the recipe (algorithm) in the comments. Then, write the code.
   
+  //Pull out the rider name
+  let nameFirst = ride.passengerDetails.first
+  let nameLast = ride.passengerDetails.last 
+
+  //get rider phone number
+  let phone = ride.passengerDetails.phoneNumber
+
+  //get address
+  let addressPickup = ride.pickupLocation.address
+  let cityPickup = ride.pickupLocation.city 
+  let statePickup = ride.pickupLocation.state 
+  let zipPickup = ride.pickupLocation.zip
+  
+  //dropoff location
+  let addressDrop = ride.dropoffLocation.address
+  let cityDrop = ride.dropoffLocation.city 
+  let stateDrop = ride.dropoffLocation.state 
+  let zipDrop = ride.dropoffLocation.zip
+
+  //get passengers
+  let numPass = ride.numberOfPassengers
+
+  //get service level
+  let service
+  if (ride.purpleRequested) {
+    service = `Purple`
+  } else if (numPass > 3) {
+    service = `XL`
+  } else {
+    service = `X`
+  }
+
+
+  //output riderinformation
+  let output = `Noober ${service} Passenger with ${numPass} riders: ${nameFirst} ${nameLast} - ${phone}. 
+  Pickup at ${addressPickup}, ${cityPickup}, ${statePickup} ${zipPickup}. Drop off at ${addressDrop}, ${cityDrop}, ${stateDrop} ${zipPickup}. `
+  console.log(output)
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
